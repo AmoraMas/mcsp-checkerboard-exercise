1eb5e2a992aa9e7ec3a87d18ab43410aa36e8298
Mel <109839793+AmoraMas@users.noreply.github.com>
2023-01-26 20:03:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,17 @@
 // Set each tile's `float` property to `left`
 // Each tile's paddingBottom is `11.1%`
 
-
+function randomColor() {
+    let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C' , 'D', 'E', 'F']
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += values[Math.floor(Math.random()*16)]
+    }
+    console.log(color);
+    return color;
+}
 
 for (let i = 1; i <= 9*7; i++) {
-    function randomColor() {
-        let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C' , 'D', 'E', 'F']
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += values[Math.floor(Math.random()*16)]
-        }
-        console.log(color);
-        return color;
-    }
-
     let square = document.createElement('div');
     square.style.width = '11.1%';
     square.style.height = '11.1%';
