7b38a4c3b13aad15d5e9151126598174af5a3ed2
AmoraMas <amoramas@protonmail.com>
2023-01-26 19:30:53 -0700

    Completed Gradient Feature

--- script.js
+++ script.js
@@ -4,17 +4,34 @@
 // Set each tile's `float` property to `left`
 // Each tile's paddingBottom is `11.1%`
 
+let color1 = '#AA0000';
+let color2 = '#00AA00';
 
+for (let i = 1; i <= 9*7; i++) {  // 9*7
+    function changeColor(currentColor) {
+        let values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C' , 'D', 'E', 'F']
+        let colorArray = currentColor.split('');
+        //console.log('v0: ' + colorArray[5]);
+        //console.log('v1: ' + values.indexOf(colorArray[5]));
+        //console.log('v2: ' + (Number(values.indexOf(colorArray[5])) + 1));
 
-for (let i = 1; i <= 9*7; i++) {
-    function randomColor() {
-        let values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C' , 'D', 'E', 'F']
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += values[Math.floor(Math.random()*16)]
+        if (values.indexOf(colorArray[6]) < 15) {
+            colorArray[6] = values[Number(values.indexOf(colorArray[6])) + 4];
         }
-        console.log(color);
-        return color;
+        else {
+            colorArray[6] = values[0];
+            if (values.indexOf(colorArray[5]) < 15) {
+                colorArray[5] = values[Number(values.indexOf(colorArray[5])) + 4];
+            }
+            else {
+                colorArray[5] = values[0];
+            }
+        }
+       let newColor = '';
+       for (let i = 0; i < 7; i++) {
+            newColor += colorArray[i];
+       }
+        return newColor;
     }
 
     let square = document.createElement('div');
@@ -22,7 +39,12 @@ for (let i = 1; i <= 9*7; i++) {
     square.style.height = '11.1%';
     square.style.float = 'left';
     square.style.paddingBottom = "11.1%"
-    square.style.backgroundColor = randomColor();
+    if (i % 2 == 0) {
+        square.style.backgroundColor = changeColor(color1);
+    }
+    else {
+        square.style.backgroundColor = changeColor(color2);
+    }
 
     document.body.appendChild(square);
 }
